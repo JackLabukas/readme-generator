c7fc74ffc126b91e1b1e578bbd26dbefe031cd99
JackLabukas <labukasgediminas@gmail.com>
2023-01-20 18:38:09 +0000

    added questions

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    message: "What is your Project Title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Your Project description",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Provide installation instructions",
+    name: "install",
+  },
+  {
+    type: "input",
+    message: "How to use the application",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Chose your licence",
+    choices: ["MIT", "Other", "GPlv2", "Apache"],
+  },
+  {
+    type: "input",
+    message: "Please provide contributors",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message: "How to test the application",
+    name: "test",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((inquirerResponse) => {
+    console.log("Please wait while generating readme file");
+    writeToFile("README.md", generateMarkdown());
+  });
 }
 
 // function call to initialize program
